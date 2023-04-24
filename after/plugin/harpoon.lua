5b7e00881a1777ce1e42920c71bfce82a985e8b7
lewiszachm <40589006+lewiszachm@users.noreply.github.com>
2023-04-24 15:16:19 -0700

    fixed typo in harpoon.lua

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -20,7 +20,7 @@ vim.keymap.set("n", "<leader>h", ui.toggle_quick_menu)
 -- leader,a adds current file to harpoon
 vim.keymap.set("n", "<leader>a", mark.add_file)
 
--- Alt-1 through Alt-0 switch between harpooned files
+-- Ctrl-1 through Ctrl-0 switch between harpooned files
 vim.keymap.set("n", "<C-1>", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<C-2>", function() ui.nav_file(2) end)
 vim.keymap.set("n", "<C-3>", function() ui.nav_file(3) end)
